ca36ece672fdd22e03e32ffda0c00d0737481bc0
Carson McKinstry <carson.mckinstry@gmail.com>
2017-05-07 00:31:37 -0500

    Styling is what is left

--- js/main.js
+++ js/main.js
@@ -7,14 +7,19 @@ function mainCtrl( $interval ) {
 
   vm.pomMin = 25;
   vm.pomTime = vm.pomMin * 60;
-  vm.breakTime = 300;
+  vm.breakMin = 5;
+  vm.breakTime = vm.breakMin * 60 + 1;
   vm.timeLeft = displayTime(vm.pomTime);
-  vm.originalTime = vm.pomTime;
-  vm.message = "Waiting..."
+  vm.originalTime = 25;
+  vm.message = "Waiting to start..."
 
   vm.start = start;
-  vm.cancel = cancel;
+  vm.pause = pause;
   vm.reset = reset;
+  vm.pomUp = pomUp;
+  vm.pomDown = pomDown;
+  vm.breakUp = breakUp;
+  vm.breakDown = breakDown;
 
   var timer = 0;
   var onBreak = false;
@@ -26,33 +31,78 @@ function mainCtrl( $interval ) {
   }
 
   function start() {
-    vm.message = "Working!";
-    running = true;
+    if(!onBreak){
+      vm.message = "Working!";
+      running = true;
 
-    tick();
-    timer = $interval(tick, 1000, 0);
+      tick();
+      timer = $interval(tick, 1000, 0);
+    } else {
+      timer = $interval(breakTick, 1000, 0);
+    }
 
     function tick() {
       vm.pomTime--;
       vm.timeLeft = displayTime(vm.pomTime);
       if ( vm.pomTime === 0){
-        vm.message = "Done!";
+        onBreak = true;
+        vm.message = "Time for a break!";
         $interval.cancel(timer);
+        breakTick();
+        timer = $interval(breakTick, 1000, 0);
       }
+    }
+
+    function breakTick() {
+      vm.breakTime--;
+      if (vm.breakTime === 0){
+        onBreak = false;
+        vm.message = "Waiting to start";
+        reset();
+      }
+      vm.timeLeft = displayTime(vm.breakTime);
     }
   }
 
-  function cancel() {
-    running = false;
+  function pause() {
+    vm.message = "Paused...";
     $interval.cancel(timer);
   }
 
   function reset() {
-    vm.message = "Waiting..."
+    vm.message = "Waiting to start"
     $interval.cancel(timer);
     running = false;
     vm.pomTime = vm.pomMin * 60;
     vm.timeLeft = displayTime(vm.pomTime);
+  }
+
+  function resetBreak(){
+    vm.breakTime = vm.breakMin * 60 +1;
+    console.log(vm.breakTime);
+  }
+
+  function pomUp() {
+    vm.pomMin++;
+    reset();
+  }
+
+  function pomDown() {
+    vm.pomMin--;
+    if (vm.pomMin < 1) {
+      vm.pomMin = 1;
+    }
+    reset();
+  }
+
+  function breakUp() {
+    vm.breakMin++;
+    resetBreak();
+  }
+
+  function breakDown() {
+    vm.breakMin--;
+    resetBreak();
   }
 
 }
